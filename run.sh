9d073ce73f064c15c426eea3b1672c188d084204
TuroTheReal <arthurbernard92000@gmail.com>
2024-11-04 11:00:44 +0100

    Minishell

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@
 dir=$(pwd)
 
 valgrind --show-leak-kinds=all --show-reachable=yes --leak-check=full --track-origins=yes --track-fds=yes --child-silent-after-fork=yes --suppressions=readline.supp ./minishell
+
+#add if needed
